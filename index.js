655910abf96c7d693cf4d30b2976ceafa3e311a7
priuzrakss <70162254+priuzrakss@users.noreply.github.com>
2025-04-14 17:34:44 +0500

    delete token_check

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const PORT = 3000;
 
-// === Настройка Телеграм-бота ===
-const token = process.env.TELEGRAM_BOT_TOKEN; // Получаем токен из переменных окружения
-const bot = new TelegramBot(token, { polling: true });
-
-
 // === Настройка статического сервера ===
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // === Запуск сервера ===
 app.listen(PORT, () => {
