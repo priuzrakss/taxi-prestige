4017366398711c4067dc21e8daa3d8b9dcdbbec5
priuzrakss <70162254+priuzrakss@users.noreply.github.com>
2025-04-14 17:34:03 +0500

    delete bot

--- index.js
+++ index.js
@@ -10,23 +10,10 @@ const PORT = 3000;
 const token = process.env.TELEGRAM_BOT_TOKEN; // Получаем токен из переменных окружения
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет! Я теперь работаю с токеном из dotenv!');
-});
 
 // === Настройка статического сервера ===
 app.use(express.static(path.join(__dirname, 'public')));
 
-// === Пример маршрута для отправки сообщения ===
-app.get('/send-message', (req, res) => {
-    const chatId = 'ВАШ_CHAT_ID';
-    const message = 'Сообщение, отправленное с сайта!';
-
-    bot.sendMessage(chatId, message)
-        .then(() => res.send('Сообщение успешно отправлено!'))
-        .catch((err) => res.status(500).send(err));
-});
 
 // === Запуск сервера ===
 app.listen(PORT, () => {
