75b635283bacb562649ba7636e491eae35e6a9c6
priuzrakss <70162254+priuzrakss@users.noreply.github.com>
2025-04-24 22:31:16 +0500

    update API endpoint for order submission to use secure HTTPS

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ app.get('/', (req, res) => {
 });
 
 // === Запуск сервера ===
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Сервер запущен на http://0.0.0.0:${PORT}`);
+app.listen(PORT, 'localhost', () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
